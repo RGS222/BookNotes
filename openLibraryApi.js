5c5a30a4ad142a87fcb0a306335b09f15eaf3cfe
RGS222 <kenyeung777@gmail.com>
2026-05-19 21:07:50 -0700

    fix url

--- openLibraryApi.js
+++ openLibraryApi.js
@@ -1,20 +1,28 @@
 import axios from "axios";
 
-const BOOK_API_URL = "https://openlibrary.org/search.json?title=$title&author=$author&lang=en";
+const BOOK_API_URL = "https://openlibrary.org/search.json?lang=en";
 const COVER_API_URL = "https://covers.openlibrary.org/b/olid/$olid-L.jpg";
 
 export async function getBookCoverUrl(title, author) {
-
-    let bookUrl = BOOK_API_URL.replace("$title", title);
-    if (!author) {
-        bookUrl = bookUrl.replace("$author", author);
+    let bookUrl = BOOK_API_URL;
+    if (title) {
+        title = title.replace(" ", "+");
+        bookUrl += "&title=" + title;
     }
+
+    if (author) {
+        author = author.replace(" ", "+");
+        bookUrl += "&author=" + author;
+    }
+
     try {
+        // console.log(bookUrl);
         const response = await axios.get(bookUrl);
         const books = response.data.docs;
         if (books) {
             const olid = books[0].cover_edition_key;
             const coverUrl = COVER_API_URL.replace("$olid", olid);
+            // console.log(coverUrl);
             return coverUrl;
         }
     }
